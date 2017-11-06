454a89c175714fae3b708c09a097b4c845d77911
floydwch <floydwch@gmail.com>
2017-11-06 18:09:11 +0800

    add some comments

--- server/TopicStore.js
+++ server/TopicStore.js
@@ -1,6 +1,7 @@
 const uuid = require('uuid/v4')
 
 
+// supoort Observer Pattern
 class TopicStore {
   constructor() {
     this.topics = []
@@ -8,6 +9,7 @@ class TopicStore {
     this.observers = new Set()
   }
 
+  // get top 20 topics
   get head() {
     return this.topics.slice().sort((a, b) => b.votes - a.votes).slice(0, 20)
   }
@@ -24,6 +26,7 @@ class TopicStore {
     this.observers.forEach(cb => cb(this.head))
   }
 
+  // push new topic to the store
   push(topic) {
     const id = uuid()
     this.topics.push({id, title: topic.title, votes: 0})
@@ -31,6 +34,8 @@ class TopicStore {
     this.notify()
   }
 
+  // vote topic by id
+  // vote := [1, -1], 1 for upvoate, -1 for downvote
   vote(id, vote) {
     const topic = this.topics[this.topic_indices[id]]
     if (vote === 1) {
--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ websocketify(app)
 
 const topic_store = new TopicStore()
 
+
+// returns top 20 topics
+// by get /api/v1/topics
 api_router.get('/topics', (req, res) => {
   res.send({
     status: 'success',
@@ -18,6 +21,8 @@ api_router.get('/topics', (req, res) => {
   })
 })
 
+// server pushes top 20 topics
+// by listen to /api/v1/topics with WebSocket
 api_router.ws('/topics', ws => {
   const observer = topics => {
     ws.send(JSON.stringify({topics}))
@@ -30,6 +35,9 @@ api_router.ws('/topics', ws => {
   })
 })
 
+// endpoint for posting a new topic
+// returns the status and message of the operation
+// by post /api/v1/topic with {title}
 api_router.post('/topic', (req, res) => {
   const {title} = req.body
 
@@ -48,6 +56,8 @@ api_router.post('/topic', (req, res) => {
   }
 })
 
+// endpoint for voting a topic by id
+// by patch /api/v1/topics/:id with {vote := [1, -1]}
 api_router.patch('/topics/:id', (req, res) => {
   topic_store.vote(req.params.id, req.body.vote)
   res.send({
